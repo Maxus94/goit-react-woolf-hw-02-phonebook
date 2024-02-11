14aa8af30a27351d532d2859a8372b077f8e86ca
Maxim <kmaxusk@gmail.com>
2024-02-11 17:00:38 +0200

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,14 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+// style={{
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   fontSize: 40,
+//   color: '#010101',
+// }}
 
 export class App extends Component {
   state = {
@@ -9,30 +19,32 @@ export class App extends Component {
   };
 
   handleSubmit = evt => {
-    evt.preventDegault();    
-    //this.createContact(this.state.name, this.state.number);
-    console.log(this.state);
+    evt.preventDefault();
+    this.state.contacts.some(({ name }) => name === this.state.name)
+      ? alert('exist')
+      : this.createContact(this.state.name, this.state.number);
   };
   handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value });    
+    this.setState({ [evt.target.name]: evt.target.value });
   };
 
   createContact = (name, number) => {
-    this.setState(prev => ({ contacts: [...prev.contacts, { name: name, number: number }] }));
+    const id = nanoid();
+    this.setState(prev => ({
+      contacts: [...prev.contacts, { name, number, id }],
+    }));
+  };
+
+  handleDelete = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
   render() {
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
+      <div>
+        <h1>Phonebook</h1>
         <form action="" onSubmit={this.handleSubmit}>
           <label htmlFor="">
             Name
@@ -58,6 +70,44 @@ export class App extends Component {
           </label>
           <button type="submit">Add contact</button>
         </form>
+        <h2>Contacts</h2>
+        <label htmlFor="">
+          Find contact by name
+          <input
+            type="text"
+            name="filter"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            onChange={this.handleChange}
+          />
+        </label>
+        {this.state.filter !== ''
+          ? this.state.contacts
+              .filter(contact => {
+                let compare = contact.name;
+                return compare.includes(this.state.filter);
+              })
+              .map(contact => (
+                <li key={contact.id}>
+                  {contact.name} {contact.number}
+                  <button
+                    type="button"
+                    onClick={() => this.handleDelete(contact.id)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))
+          : this.state.contacts.map(contact => (
+              <li key={contact.id}>
+                {contact.name} {contact.number}
+                <button
+                  type="button"
+                  onClick={() => this.handleDelete(contact.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
       </div>
     );
   }
