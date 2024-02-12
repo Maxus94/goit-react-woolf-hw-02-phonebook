e94de236262ea3b3011e87ba4acb53dd7f229a1a
Maxim <kmaxusk@gmail.com>
2024-02-12 16:23:41 +0200

    task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,6 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-// style={{
-//   height: '100vh',
-//   display: 'flex',
-//   justifyContent: 'center',
-//   alignItems: 'center',
-//   fontSize: 40,
-//   color: '#010101',
-// }}
-
 export class App extends Component {
   state = {
     contacts: [],
@@ -24,13 +15,34 @@ export class App extends Component {
   };
 
   createContact = (name, number) => {
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert('exist');
+      return;
+    }
     const id = nanoid();
     this.setState(prev => ({
       contacts: [...prev.contacts, { name, number, id }],
     }));
   };
 
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
+  handleFilter = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+    return this.state.contacts.filter(contact => {
+      let compare = contact.name;
+      return compare.includes(evt.target.value);
+    });
+  };
 
   handleDelete = id => {
     this.setState(prev => ({
@@ -42,47 +54,16 @@ export class App extends Component {
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm contacts={this.state.contacts} createContact={this.createContact}/>
+        <ContactForm
+          contacts={this.state.contacts}
+          createContact={this.createContact}
+        />
         <h2>Contacts</h2>
-        <Filter handleChange={this.handleChange}/>
-        <ContactList contacts = {this.state.contacts}/>
-        {/* <label htmlFor="">
-          Find contact by name
-          <input
-            type="text"
-            name="filter"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            onChange={this.handleChange}
-          />
-        </label>
-        {this.state.filter !== ''
-          ? this.state.contacts
-              .filter(contact => {
-                let compare = contact.name;
-                return compare.includes(this.state.filter);
-              })
-              .map(contact => (
-                <li key={contact.id}>
-                  {contact.name} {contact.number}
-                  <button
-                    type="button"
-                    onClick={() => this.handleDelete(contact.id)}
-                  >
-                    Delete
-                  </button>
-                </li>
-              ))
-          : this.state.contacts.map(contact => (
-              <li key={contact.id}>
-                {contact.name} {contact.number}
-                <button
-                  type="button"
-                  onClick={() => this.handleDelete(contact.id)}
-                >
-                  Delete
-                </button>
-              </li>
-            ))} */}
+        <Filter handleChange={this.handleChange} />
+        <ContactList
+          contacts={this.filterContacts()}
+          handleDelete={this.handleDelete}
+        />
       </div>
     );
   }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,45 @@
 import { Component } from 'react';
+import css from './ContactForm.module.css'
 export class ContactForm extends Component {
   state = {
     name: '',
     number: '',
   };
-  
+
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
   handleSubmit = evt => {
     evt.preventDefault();
-    console.log(this.state);    
-    this.props.contacts.some(({ name }) => name === this.state.name)
-      ? alert('exist')
-      : this.props.createContact(this.state.name, this.state.number);
+    this.props.createContact(this.state.name, this.state.number);
+    this.setState({name:'', number: ''});
+
   };
   render() {
     return (
-      <form action="" onSubmit={this.handleSubmit}>
+      <form className={css.formStyle} action="" onSubmit={this.handleSubmit}>
         <label htmlFor="">
           Name
-          <input
+          <input className={css.formInput}
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            value = {this.state.name}
             onChange={this.handleChange}
           />
         </label>
         <label htmlFor="">
           Number
-          <input
+          <input className={css.formInput}
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            value={this.state.number}
             onChange={this.handleChange}
           />
         </label>
